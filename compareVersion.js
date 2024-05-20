a735bf2ff710bd5390d3b6aecbbbf8d4bba968a5
zolteak <144640967+zolteak@users.noreply.github.com>
2024-05-20 19:08:31 +0800

    Update compareVersion.js

--- compareVersion.js
+++ compareVersion.js
@@ -1,21 +1,24 @@
 const compareVersion = (version1, version2) => {
+    let result = 0;
+    
     if (version1.length === version2.length) {
-        if (parseFloat(version1) === parseFloat(version2)) {
-            return 0;
-        }
-        else if (parseFloat(version1) > parseFloat(version2)) {
-            return 1;
-        } 
-        else {
-            return -1;
+        result = 0;
+        
+        if (parseFloat(version1) > parseFloat(version2)) {
+            result = 1;
+        }  
+        if (parseFloat(version1) < parseFloat(version2)) {
+            result = -1;
         }
     }
-    else if (version1.length > version2.length) {
-        return 1;
-    } 
-    else {
-        return -1;
+    if (version1.length > version2.length) {
+        result = 1;
+    }     
+    if (version1.length < version2.length) {
+        result = -1;
     }
+    
+    return result;
 };
 
 exports.compareVersion = compareVersion;
