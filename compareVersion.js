01d992d921afef0e940f7b7b9600bd7c55ddc6a8
zolteak <144640967+zolteak@users.noreply.github.com>
2024-05-23 09:40:31 +0800

    Update compareVersion.js

--- compareVersion.js
+++ compareVersion.js
@@ -1,20 +1,19 @@
 const compareVersion = (version1, version2) => {
     let result = 0;
     
-    if (version1.length === version2.length) {
-        result = 0;
-        
-        if (parseFloat(version1) > parseFloat(version2)) {
+    if (version1.slice(0, version1.indexOf('.')) === version2.slice(0, version2.indexOf('.'))) {              
+        if (parseFloat(version1.slice(version1.indexOf('.') + 1)) > version2.slice(version2.indexOf('.') + 1)) {
             result = 1;
         }  
-        if (parseFloat(version1) < parseFloat(version2)) {
+        if (parseFloat(version1.slice(version1.indexOf('.') + 1)) < version2.slice(version2.indexOf('.') + 1)) {
             result = -1;
         }
+        
     }
-    if (version1.length > version2.length) {
+    if (version1.slice(0, version1.indexOf('.')) > version2.slice(0, version2.indexOf('.'))) {
         result = 1;
     }     
-    if (version1.length < version2.length) {
+    if (version1.slice(0, version1.indexOf('.')) < version2.slice(0, version2.indexOf('.'))) {
         result = -1;
     }
     
